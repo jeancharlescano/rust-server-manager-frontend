91b05ea736b8129a950dc950310c0fce22f38815
Jean-Charles <jeancharle.cano@outlook.fr>
2022-08-15 13:05:11 +0200

    �️�️⚒️ Fix import, header ServerListScreen

--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -1,15 +1,15 @@
 import logo from "../../assets/img/rust-logo.jpg";
-import "./HomeScreen.css";
+import styles from "./HomeScreen.module.scss";
 import { Link } from "react-router-dom";
 
 function HomeScreen() {
   return (
-    <header className="screen">
-      <div className="center">
+    <header className={styles.screen}>
+      <div className={styles.center}>
         <Link to="/servers">
-          <img src={logo} className="logo" alt="logo" />
+          <img src={logo} className={styles.logo} alt="logo" />
         </Link>
-        <p className="clignote">👆 click here 👆</p>
+        <p className={styles.clignote}>👆 click here 👆</p>
       </div>
     </header>
   );
--- src/screens/serverListScreen/ServerListScreen.js
+++ src/screens/serverListScreen/ServerListScreen.js
@@ -1,14 +1,17 @@
 import React from "react";
+import styles from "./ServerListScreen.module.scss";
 import logo from "../../assets/img/rust-logo.jpg";
 import { Link } from "react-router-dom";
 
 const ServerListScreen = () => {
   return (
-    <header>
-      <Link to="/">
-        <img src={logo} alt="logo" />
-      </Link>
-    </header>
+    <div className={styles.body}>
+      <div className={styles.header}>
+        <Link to="/">
+          <img src={logo} className={styles.logoRust} alt="logo" />
+        </Link>
+      </div>
+    </div>
   );
 };
 
